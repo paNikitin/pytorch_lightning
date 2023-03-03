c7911fa80e4477dc1892ae36fa050c6a075a38af
paNikitin <115797306+paNikitin@users.noreply.github.com>
2023-03-03 11:03:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ import fmnist_data
 import pytorch_lightning as pl
 import hydra
 from config import FMNISTConfig
-from hydra.core.config_store import ConfigStore
-
-cs = ConfigStore.instance()
-cs.store(name="fmnist_config", node=FMNISTConfig)
 
 @hydra.main(config_path="configs", config_name="config", version_base=None)
 def main(cfg: FMNISTConfig):
+    #a = torch.optim.Optimizer(params=None, defaults=)
     model = fmnist_classifier.LightningFMNISTClassifier(cfg.params.optimizer, cfg.params.lr)
     trainer = pl.Trainer( max_epochs=cfg.params.max_epochs)
     data_module = fmnist_data.FMNISTDataModule(cfg.params.batch_size)
@@ -17,6 +14,5 @@ def main(cfg: FMNISTConfig):
     #model.eval()
     #trainer.predict(model, data_module)
     #print(model.get_accuracy())
-    
 if __name__ == "__main__":
     main()
